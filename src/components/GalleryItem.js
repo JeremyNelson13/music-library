35dc71f447ad6f5e2a403f7aec2e9e4d13e84508
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-03-19 09:57:57 -0400

    adding artist and album views, adding routes

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function GalleryItem(props) {
     let [view, setView] = useState(false)
@@ -10,7 +11,7 @@ function GalleryItem(props) {
     }
 
     const detailStyle = {
-        'width':'80vw',
+        'width': '80vw',
         'height': '20vh',
         'border': '1px solid black',
         'margin': '2px',
@@ -21,7 +22,7 @@ function GalleryItem(props) {
     }
 
     const simpleView = () => {
-        return(
+        return (
             <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
@@ -29,18 +30,27 @@ function GalleryItem(props) {
         )
     }
     const detailView = () => {
-        return(
+        return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
         )
     }
     return (
-        <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
-           {view ? detailView() : simpleView()}
+        <div onClick={() => setView(!view)} style={{ 'display': 'inline-block' }}>
+            {view ? detailView() : simpleView()}
         </div>
     )
 }
